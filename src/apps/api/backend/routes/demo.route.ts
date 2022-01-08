2543d43f79980c704891e61293b03ef64b75c0fd
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-01-08 13:25:26 -0600

    :construction: -Add status

--- src/apps/api/backend/routes/demo.route.ts
+++
@@ -1,4 +0,0 @@
-import { Router, Request, Response } from 'express'
-export const register = (router: Router) => {
-  router.get('/status', (req: Request, res: Response) => res.status(201).send())
-}
