9e569f51f70bb4271d63971634763ed5b820c001
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-01-14 22:57:30 -0600

    :recycle: -Add contructor in userCreator

--- src/Contexts/Api/Users/application/UserCreator.ts
+++ src/Contexts/Api/Users/application/UserCreator.ts
@@ -2,7 +2,12 @@ import { User } from '../domain/User'
 import { UserRepository } from '../domain/UserRepository'
 
 export class UserCreator {
-  constructor (private repository: UserRepository) {}
+  private readonly repository: UserRepository
+
+  constructor (repository: UserRepository) {
+    this.repository = repository
+  }
+
   async run (id: string, name: string, email: string, password: string) {
     const user = new User(id, name, email, password)
 
