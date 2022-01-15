f9b618d139efcf6b5434d9abfbcdb3afa1b2f45a
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-01-14 22:46:19 -0600

    :recycle: -Add express validator

--- src/apps/api/backend/routes/index.ts
+++ src/apps/api/backend/routes/index.ts
@@ -1,5 +1,7 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
+import { validationResult, ValidationError } from 'express-validator'
 import glob from 'glob'
+import httpStatus from 'http-status'
 import path from 'path'
 
 export function registerRoutes (router: Router) {
@@ -10,4 +12,16 @@ export function registerRoutes (router: Router) {
 function register (routePath: string, router: Router) {
   const route = require(routePath)
   route.register(router)
+}
+
+export function validateReqSchema (req: Request, res: Response, next: Function) {
+  const validationErrors = validationResult(req)
+  if (validationErrors.isEmpty()) {
+    return next()
+  }
+  const errors = validationErrors.array().map((err: ValidationError) => ({ [err.param]: err.msg }))
+
+  return res.status(httpStatus.UNPROCESSABLE_ENTITY).json({
+    errors
+  })
 }
--- tests/apps/api/backend/features/users/create-user.feature
+++ tests/apps/api/backend/features/users/create-user.feature
@@ -4,10 +4,21 @@ Feature: Create new user
         Given I send a PUT request to "/users/ef8ac118-8d7f-49cc-abec-78e0d05af80a" with body:
         """
         {
-            "name": "Sergio",
+            "name": "sergio",
             "email": "sergio@demo.com",
             "password": "123456"
         }
         """
         Then the response status code should be 201
         And the response should be empty
+    
+    Scenario: An invalid no existing user
+        Given I send a PUT request to "/users/ef8ac118-8d7f-49cc-abec-78e0d05af80b" with body:
+        """
+        {
+            "name": 4,
+            "email": 3,
+            "password": 1
+        }
+        """
+        Then the response status code should be 422
