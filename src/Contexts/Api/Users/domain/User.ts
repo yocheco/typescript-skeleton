f5c33436cd43cd536c180edf39b3ee7d7028d03d
Sergio Sandoval <yo.sergio.sandoval@gmail.com>
2022-01-24 18:42:04 -0600

    :recycle: -Add value object test to user

--- src/Contexts/Api/Users/domain/User.ts
+++ src/Contexts/Api/Users/domain/User.ts
@@ -1,12 +1,15 @@
 import { UserId } from '../../Shared/domain/Users/value-object/UserId'
+import { UserName } from './value-objects/UserName'
+import { UserEmail } from './value-objects/UserEmail'
+import { UserPassword } from './value-objects/UserPassword'
 
 export class User {
     readonly id: UserId
-    readonly name: string
-    readonly email: string
-    readonly password: string
+    readonly name: UserName
+    readonly email: UserEmail
+    readonly password: UserPassword
 
-    constructor ({ id, name, email, password }:{id: UserId, name: string, email: string, password: string }) {
+    constructor ({ id, name, email, password }:{id: UserId, name: UserName, email: UserEmail, password: UserPassword }) {
       this.id = id
       this.name = name
       this.email = email
--- tests/Contexts/Api/Users/infrastructure/persistence/FileUserRepository.test.ts
+++ tests/Contexts/Api/Users/infrastructure/persistence/FileUserRepository.test.ts
@@ -1,9 +1,18 @@
 import { User } from '../../../../../../src/Contexts/Api/Users/domain/User'
+import { UserEmail } from '../../../../../../src/Contexts/Api/Users/domain/value-objects/UserEmail'
+import { UserName } from '../../../../../../src/Contexts/Api/Users/domain/value-objects/UserName'
+import { UserPassword } from '../../../../../../src/Contexts/Api/Users/domain/value-objects/UserPassword'
 import { FileUserRepository } from '../../../../../../src/Contexts/Api/Users/infrastructure/persistence/FileUserRepository'
 import { Uuid } from '../../../../../../src/Contexts/Shared/domain/value-object/Uuid'
 describe('FileUserRepository', () => {
   it('should save a user in file', async () => {
-    const expectedUser = new User({ id: new Uuid('f3b750a5-3cc7-49a8-9645-fc3c3d832191'), name: 'name', email: 'email', password: 'password' })
+    const expectedUser = new User({
+      id: new Uuid('f3b750a5-3cc7-49a8-9645-fc3c3d832191'),
+      name: new UserName('name'),
+      email: new UserEmail('sergio@email.com'),
+      password: new UserPassword('password')
+    })
+
     const repository = new FileUserRepository()
 
     await repository.save(expectedUser)
